2d01ee6dcd86b2a825dcb509ec98ca1019bb3d09
Matthew Nguyen <matthewnguyen@csilvm-03.cs.ucsb.edu>
2022-12-02 18:25:09 -0800

    fixed cache size function

--- cache-test-skel.c
+++ cache-test-skel.c
@@ -5,15 +5,15 @@ Lab 6 - Mystery Caches
 Mystery Cache Geometries:
 mystery0:
     block size = 64 bytes
-    cache size = 4096 KB
+    cache size = 4194304 bytes = 4096 KB
     associativity = 16
 mystery1:
     block size = 4 bytes
-    cache size = 4 KB
+    cache size = 4096 bytes = 4 KB
     associativity = 1
 mystery2:
     block size = 32 bytes
-    cache size = 4.03 KB
+    cache size = 4096 bytes = 4 KB
     associativity = 128
 */
 
@@ -24,33 +24,43 @@ mystery2:
 
 /* 
    Returns the size (in B) of the cache
+
+   For each iteration, flush cache then initialize at address 0. Then fill every block that we know exist. If address 0 doesn't get rewritten, then we know that an additional block exists.
+   Iterate until address 0 gets rewritten and we will receive the number of blocks. Multiple number of blocks by block size to get capacity.
 */
-// Flush cache then do one access from address 0. Then fill every block in the cache until we reach
-// the beginning of the cache again and access cache with address 0 misses. This indicates that we
-// filled the entire cache so we can return size.
+
 int get_cache_size(int block_size) {
-  int cachesize = 0;
-  int check = block_size;
-  flush_cache();
-  access_cache(0);
+
+  int numBlocks = 0;
+
+  // fill up empty cache as much as possible per iteration -> counting number of blocks until address 0 gets overwritten. 
   while (access_cache(0)) {
-    cachesize = block_size;
-    for (int x = cachesize; x <= check; x += block_size) {
-      cachesize += block_size;
-      access_cache(cachesize);
+    flush_cache();
+    access_cache(0);
+
+    // address 0 not overwritten, must mean there is at least one more block
+    numBlocks++;
+
+    // fill up all known existing blocks in cache
+    int fill = 1;
+    while (fill <= numBlocks) {
+      access_cache(fill*block_size);
+      fill++;
     }
-    check += block_size;
   }
-  
-  return cachesize;
+
+  return numBlocks * block_size; 
 }
+
 //
 /*
    Returns the associativity of the cache
+
+   Flush cache then do one access from address 0. Then do another access from an address a cache size
+   away. This will fill the next way if there is another. Keep doing this until access cache with address 0
+   misses. This indicates that all ways have been filled and we can return the associativity
 */
-// Flush cache then do one access from address 0. Then do another access from an address a cache size
-// away. This will fill the next way if there is another. Keep doing this until access cache with address 0
-// misses. This indicates that all ways have been filled and we can return the associativity.
+
 int get_cache_assoc(int size) {
   int assoc = 0;
   flush_cache();
@@ -61,24 +71,22 @@ int get_cache_assoc(int size) {
     }
     assoc++;
   }
-
   return assoc;
 }
 
 /*
    Returns the size (in B) of each block in the cache.
+   Do one access to cache from address 0 then do accesses in increments of 1 until a miss
+   that indicates the end of the first block
 */
-// Do one access to cache from address 0 then do accesses in increments of 1 until a miss
-// that indicates the end of the first block
+
 int get_block_size() {
-  int x = 0;
-  int size = 0;
-  access_cache(x);
-  while (access_cache(x)){
-    size++;
-    x++;
+  int blockSize = 0;
+  access_cache(0);
+  while (access_cache(blockSize)){
+    blockSize++;
   }
-  return x;
+  return blockSize;
 }
 
 int main(void) {
